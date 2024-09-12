b519d728018e81e531d9cdaf8e6a094cae886ffb
Mtduffey36 <michael.t.duffey@gmail.com>
2024-09-11 19:32:42 -0500

    added custom colors in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,24 @@
+const autoprefixer = require('autoprefixer');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
   content: [
     './pages/**/*.{html,js}',
     './components/**/*.{html,js}',
     './public/index.html',
   ],
+  theme: {
+    colors: {
+      'rosy-brown': '#C2A29D',
+      'khaki': '#B5A283',
+      'dim-gray': '#64696D',
+      'charcoal': '3D4750',
+      'timberwolf': '#CDD3D1',
+    },
+  },
   // ...
 }
