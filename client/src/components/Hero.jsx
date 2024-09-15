502a091a4c9e255acfd428a5c1aa86a684b39af6
Mtduffey36 <michael.t.duffey@gmail.com>
2024-09-15 18:38:08 -0500

    changed background and title style

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <section className="max-w-7xl mx-auto border-b-2">
             <div className="flex flex-col items-center my-20">
-                <h1 id="logo" className="text-6xl lg:text-[10rem] p-2 uppercase font-bold font-text-fraunces">
+                <h1 id="logo" className="text-6xl lg:text-[10rem] p-2 uppercase font-text-fraunces">
                     {HERO_CONTENT.title}
                 </h1>
                 <p className="lg:mt-6 text-medium mb-4 font-medium tracking-tighter">
