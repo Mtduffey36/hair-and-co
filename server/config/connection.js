24d46ad25ff93ad6c18f7913679c13e6efb09fb7
Mtduffey36 <michael.t.duffey@gmail.com>
2024-09-23 19:15:08 -0500

    fixed env erro

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require ('dotenv').config();
+// import 'dotenv/config'
 
 mongoose.connect(process.env.MONGODB_URI);
+console.log("PROCESS HERE", process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
